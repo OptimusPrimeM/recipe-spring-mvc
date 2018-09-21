867a80e92bdb33278ccf5b870cb1c5ac788a2abc
Menik <menikdisney@gmail.com>
2018-09-21 10:29:47 +0600

    Initial commit

---
+++ src/main/java/com/optimusprime/recipespringmvc/RecipeSpringMvcApplication.java
@@ -0,0 +1,12 @@
+package com.optimusprime.recipespringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeSpringMvcApplication.class, args);
+	}
+}
